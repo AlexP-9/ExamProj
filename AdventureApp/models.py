6255b2d3e6c613a227d5193a8a1ec2ae84cb5423
A <test@test.com>
2025-04-24 21:27:15 +0200

    Added some views, changed the user model

--- AdventureApp/models.py
+++ AdventureApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
-class Customer(AbstractUser):
+
+class Customer(User):
     phone=models.CharField(
         max_length=32,
         validators=[
--- AdventureApp/views.py
+++ AdventureApp/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+"""
+#Registration, Login, Logout
+"""
+def view_login(request):
+    if(request.method=="POST"):
+        uname=request.POST.get("username")
+        passw=request.POST.get("password")
+        user=authenticate(request,username=uname,password=passw)
+        if user:    #if user is not None:
+            login(request,user)
+            return redirect(request.GET.get("next",""))
+        #messages.add_message(request, messages.WARNING, "Incorrect password!")
+        return render(request,"LogIn.html",{
+            "username":uname            
+        })
+    return render(request, "LogIn.html")
 
-qerghqerrughqeriguh
+def view_logout(request):
+    logout(request)
+    return redirect("")
+
+def view_register(request):
+    return render()
+
+"""
+#Trip views (all, individual, etc.)
+"""
+def view_mainpage(request):
+    return render(request, "MainPage.html")
+
+def view_trip(request, tid):
+    return render(request, "TripPage.html")
+
+"""
+#Administrative views
+"""
+
